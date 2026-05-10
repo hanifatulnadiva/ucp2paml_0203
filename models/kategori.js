fc789dccc93ebcd07d714fcfc7ac778b3371f48d
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-05-10 16:29:22 +0700

    menambahkan sequelize untuk kategori

---
+++ models/kategori.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Kategori extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Kategori.init({
+    jenis_mobil: DataTypes.STRING,
+    allowNull: false,
+      unique: true,
+      validate: {
+        isJenisMobil: true 
+      }
+  }, {
+    sequelize,
+    modelName: 'Kategori',
+  });
+  return Kategori;
+};
